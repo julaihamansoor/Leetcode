9fc5e9de36a5e4d612bf5b5fca0129c1e43a7d1f
julaihamansoor <145969094+julaihamansoor@users.noreply.github.com>
2026-09-05 10:40:43 +0530

    Time: 6 ms (16.98%) | Memory: 48.8 MB (30.52%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] countBits(int n) {
+
+
+        int arr[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+
+            int num=i;
+            int count=0;
+
+            while(num!=0){
+                if((num&1)==1){
+                    count=count+1;
+                }
+                num=num>>1;
+            }
+            arr[i]=count;
+        }
+        return arr;
+        
+    }
+}
